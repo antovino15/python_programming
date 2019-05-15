c00587c13f007fd650c27ba6cae8e46ae66c51a8
Antovino <antovino15@gmail.com>
2019-05-15 14:00:24 -0700

    Phase1

--- Factorial.py
+++ Factorial.py
@@ -1,9 +1,19 @@
-#Getting Input
-num = int(input("Enter The number: "))
-#Defing Factor
-Factor = 1
-for fact in range(1, num+1):
-    # print(num)
-    Factor *= fact
-    # print (Factor)
-print (f'Factorial of {num} is {Factor}')
+# #Getting Input
+# num = int(input("Enter The number: "))
+# #Defing Factor
+# Factor = 1
+# for fact in range(1, num+1):
+#     # print(num)
+#     Factor *= fact
+#     # print (Factor)
+# print (f'Factorial of {num} is {Factor}')
+
+#using function
+def Factorial(num):
+    if num == 0:
+        return 1
+    return num*Factorial(num -1)
+# print('Enter the numer: ')
+num = int(input('Enter the numer: '))
+
+print (Factorial(num))
