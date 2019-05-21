6f9e763829fb534ddcf878b75339484f988b04e5
Antovino <antovino15@gmail.com>
2019-05-21 14:46:36 -0700

    regex_without_saving to file

--- email_search_regex.py
+++ email_search_regex.py
@@ -1,9 +1,15 @@
 import re
 # file_to_search = open('us-500.csv', 'r')
 # print(file_to_search.readlines())
+# newfile = open('mailist.txt', 'w')
 with open("us-500.csv") as f:
     for line in f:
-        email_search = re.search(r'([\w.-]+@+[\w.]+)', line)
+        email_search = re.findall(r'([\w.-]+@+[\w.]+)', line)
         print(email_search)
+        # email_list.append(email_search)
+        # print(email_search)
+        # newfile.write(str(email_search))
 # file_to_search.close()
 # Ned to save this output in csv file---tomorrow
+# newfile.close()
+# print(email_list)
